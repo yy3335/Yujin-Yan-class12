a6630a92a022bf4d24e67c88aba12f0a967bbd88
yy3335 <165346669+yy3335@users.noreply.github.com>
2024-04-13 14:27:13 -0400

    Create BondDuration_File.py

---
+++ BondDuration_File.py
@@ -0,0 +1,16 @@
+import numpy as np
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    pmt = face * couponRate / ppy
+    times = np.arange(1, m * ppy + 1)
+    dfs = (1 + y / ppy) ** times
+    cf = np.full(m * ppy, pmt)
+    cf[-1] += face  
+    
+    pv = cf / dfs
+    
+    duration = np.sum(times * pv) / np.sum(pv)
+    duration /= ppy  
+    
+    return duration
+
+
